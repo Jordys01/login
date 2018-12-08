1fc8993293a554f264ea008958e187658f6cb1d5
jeiDev <joseanyeloserrano@gmail.com>
2018-12-08 19:09:35 -0300

    Animation in login

--- js/main.js
+++ js/main.js
@@ -15,6 +15,22 @@ function login(){
 		
 		document.querySelector(".login").style.display = "flex"
 		document.querySelector(".box-login").classList.add("box-animation")
+
+		setTimeout(()=>{
+			document.querySelector(".box-login").classList.add("width-animation")
+			setTimeout(()=>{
+				let input = document.querySelectorAll(".box-login>input"),
+					div = document.querySelectorAll(".box-login>input")
+
+				for (var i = input.length - 1; i >= 0; i--) {
+					input[i].classList.add("left-animation")
+				}
+
+				for (var i = div.length - 1; i >= 0; i--) {
+					div[i].classList.add("left-animation")
+				}
+			},1000)
+		},1000)
 	},1000)
 	
 } 
