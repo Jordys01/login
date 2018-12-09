335c966108691d1eeef03723ee033d6b35422fa5
jeiDev <joseanyeloserrano@gmail.com>
2018-12-09 03:05:20 -0300

    Improvement

--- js/main.js
+++ js/main.js
@@ -1,39 +1,56 @@
 
+var init = true,
+	ejecutRegister = true,
+	ejecutLogin = true;
+
 document.querySelector(".button-login").onclick = () =>{
-	login();
+	animationBarRegister("remove");
+	animationInputRegister("remove");
+
+	if(ejecutLogin == true){
+		ejecutLogin = false
+		ejecutRegister = false
+		login();
+	}
 }
 
 document.querySelector(".button-register").onclick = () =>{
-	register();
+	animationBarLogin("remove");
+	animationInputLogin("remove");
+
+	if(ejecutRegister == true){
+		ejecutRegister = false
+		ejecutLogin = false
+		register();
+	}
+	
 }
 
 function login(){
 	let btnLogin = document.querySelector(".button-login"),
 		btnRegister = document.querySelector(".button-register"),
-		register = document.querySelector(".register"),
-		login = document.querySelector(".login")
+		box_login = document.querySelector(".box-login"),
+		login = document.querySelector(".login"),
+		register = document.querySelector(".register")
 
-	animationInputLogin("remove");
-	animationBarLogin("remove");
-
-	document.querySelector(".box-login").style.padding = "0px"
 	login.classList.remove("fade-out")
 	register.classList.add("fade-out")
-	register.style.diplay = "none"
-	btnLogin.classList.remove("btn-login-animation")
 	btnLogin.classList.add("button-login-animation")
 	btnLogin.querySelector("i").classList.add("fade-out")
 	btnLogin.querySelector("span").classList.add("fade-out")
 
 	setTimeout(()=>{
-		btnLogin.style.display = "none"
 		register.style.display = "none"
-		btnRegister.querySelector("i").classList.remove("fade-out")
-		btnRegister.querySelector("span").classList.remove("fade-out")
-		btnRegister.style.display = "flex"
+		document.querySelector(".box-register-default").classList.remove("box-register-default")
+	},500)
+
+	setTimeout(()=>{
+		btnLogin.style.display = "none"
 		login.style.display = "flex"
-		document.querySelector(".box-login").classList.add("box-animation")
+		btnRegister.style.opacity = "0"
+		btnRegister.style.display = "flex"
 		btnRegister.classList.add("btn-register-animation")
+		box_login.classList.add("box-animation")
 	},1000);
 
 	setTimeout(()=>{
@@ -42,11 +59,21 @@ function login(){
 
 	setTimeout(()=>{		
 		animationInputLogin("add");
-	},2000);
+	},3000);
 
 	setTimeout(()=>{		
 		animationBarLogin("add");
-	},2000);
+	},4000);
+
+	setTimeout(()=>{
+		ejecutRegister = true
+		document.querySelector(".box-login").classList.add("box-login-default")
+		btnRegister.style.opacity = "1"
+		btnLogin.classList.remove("button-login-animation")
+		btnLogin.querySelector("i").classList.remove("fade-out")
+		btnLogin.querySelector("span").classList.remove("fade-out")
+		btnRegister.classList.remove("btn-register-animation")
+	},5000)
 	
 } 
 
@@ -54,42 +81,56 @@ function register(){
 	let btnRegister = document.querySelector(".button-register"),
 		btnLogin = document.querySelector(".button-login"),
 		register = document.querySelector(".register"),
+		box_register = document.querySelector(".box-register"),
 		login = document.querySelector(".login")
-
-	animationInputRegister("remove");
-	animationBarRegister("remove");
-
-	login.classList.add("fade-out")
+	
 	register.classList.remove("fade-out")
-	login.style.display = "none"
-	btnRegister.classList.remove("btn-register-animation")
+	login.classList.add("fade-out")
 	btnRegister.classList.add("button-register-animation")
 	btnRegister.querySelector("i").classList.add("fade-out")
 	btnRegister.querySelector("span").classList.add("fade-out")
 
 	setTimeout(()=>{
-		btnRegister.style.display = "none"
 		login.style.display = "none"
+		document.querySelector(".box-login-default").classList.remove("box-login-default")
+	},500)
+
+	setTimeout(()=>{
+		btnRegister.style.display = "none"
 		btnLogin.style.display = "flex"
 		btnLogin.querySelector("i").classList.remove("fade-out")
 		btnLogin.querySelector("span").classList.remove("fade-out")
-		document.querySelector(".register").style.display = "flex"
-		document.querySelector(".box-register").classList.add("box-animation-register")
 		btnLogin.classList.add("btn-login-animation")
+
+		document.querySelector(".register").style.display = "flex"
+		box_register.classList.add("box-animation")
 	},1000);
 
 	setTimeout(()=>{
-		document.querySelector(".box-register").classList.add("width-animation")
+		box_register.classList.add("width-animation")
 	},2000);
 
 	setTimeout(()=>{		
 		animationInputRegister("add");
-	},2000);
+	},3000);
 
 	setTimeout(()=>{		
 		animationBarRegister("add");
-	},2000);
+	},4000);
+
+	setTimeout(()=>{		
+		ejecutLogin = true
+		box_register.classList.add("box-register-default")
+		box_register.classList.remove("box-animation")
+		box_register.classList.remove("width-animation")
+		btnLogin.classList.remove("btn-login-animation")
+		btnRegister.classList.remove("button-register-animation")
+		btnRegister.querySelector("i").classList.remove("fade-out")
+		btnRegister.querySelector("span").classList.remove("fade-out")
+	},4500);
 }
+
+
 
 function animationInputLogin(type){
 	let input = document.querySelectorAll(".box-login>input")
